13283f26a1a0927035570924b5ff50ee607c1b51
joisse1101 <thoiwei@gmail.com>
2026-08-21 10:09:50 +0800

    fix: build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/main.scss';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
