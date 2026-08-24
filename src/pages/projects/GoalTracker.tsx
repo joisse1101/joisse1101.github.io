d492a3f28f2545d2b6a37abbd9a4723ff82e2925
joisse1101 <thoiwei@gmail.com>
2026-08-24 21:00:43 +0800

    fix: fallback function for crypto.randomUUID - crypto.randomUUID is not available for web apps in Android over http - add fallbacks in custom generateUUID function

--- src/pages/projects/GoalTracker.tsx
+++ src/pages/projects/GoalTracker.tsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 import { TrackerTab } from "@/components/partials/goalTracker/TrackerTab";
 import { getStorageItem } from "@/utils/storage";
 import { useGoalTitle, deleteGoalStorage } from "@/hooks/useGoalTracker";
+import { generateUUID } from '@/utils/numbers';
 
 const STORAGE_KEY = 'goal_tracker_tab_ids';
 
 export default function GoalTracker() {
-    const [tabIds, setTabIds] = useState<string[]>(getStorageItem(STORAGE_KEY, [crypto.randomUUID()]));
+    const [tabIds, setTabIds] = useState<string[]>(() =>
+        getStorageItem(STORAGE_KEY, [generateUUID()]));
     const [activeTab, setActiveTab] = useState<string>(tabIds[0]);
 
     useEffect(() => {
@@ -17,12 +19,12 @@ export default function GoalTracker() {
 
     const tabItems: TabItem[] = tabIds.map((id) => ({
         id: id,
-        label: <DynamicTabLabel id={id} />, // Or pull dynamic titles if managed globally
+        label: <DynamicTabLabel id={id} />,
         content: <TrackerTab id={id} />,
     }));
 
     function handleAddTab() {
-        const newId = crypto.randomUUID();
+        const newId = generateUUID();
         setTabIds([...tabIds, newId]);
         setActiveTab(newId);
     }
