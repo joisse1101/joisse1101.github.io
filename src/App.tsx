4c47a7320383da4085fda51d19fe5e640fe22b6b
joisse1101 <thoiwei@gmail.com>
2026-08-25 19:23:36 +0800

    feat: add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/main.scss';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 import GrannySquare from './pages/projects/GrannySquare';
-// import About from './pages/About';
 import GoalTracker from './pages/projects/GoalTracker';
 import { Toaster } from 'sonner';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter> 
       <Toaster />
       <Routes>
         {/* Parent route using the layout */}
@@ -18,6 +19,7 @@ export default function App() {
           {/* <Route path="about" element={<About />} /> */}
           <Route path="/projects/granny-square" element={<GrannySquare />} />
           <Route path="/projects/goal-tracker" element={<GoalTracker />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
